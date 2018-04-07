91a71f290240a74b81e7d90ca77574b85d1ac935
Corbin Black <corbinblack@Corbins-MBP.local>
2018-04-06 19:59:37 -0500

    First Commit

---
+++ app/config.py
@@ -0,0 +1,12 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktaskr.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+CSRF_ENABLED = True
+SECRET_KEY = 'my_super_precious'
+
+
+DATABASE_PATH = os.path.join(basedir, DATABASE)
